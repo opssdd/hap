db0c6baf88188f0240237413a4d536cf00cf0c4f
opssdd <61148520+opssdd@users.noreply.github.com>
2020-03-09 09:18:49 +0530

    Add files via upload

---
+++ Cyberkallan.py
@@ -0,0 +1,66 @@
+import platform
+import os
+import time
+import random
+import urllib.request
+
+def banner():
+    if platform.system().lower()=="windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+    
+                       
+print(r"               _                         _              _   _                  ")
+print(r"              | |                       | |            | | | |                 ")
+print(r"   ___   _   _  | |__     ___   _ __    | | __   __ _  | | | |   __ _   _ __   ")
+print(r"  / __| | | | | | '_ \   / _ \ | '__|   | |/ /  / _` | | | | |  / _` | | '_ \  ")
+print(r" | (__  | |_| | | |_) | |  __/ | |      |   <  | (_| | | | | | | (_| | | | | | ")
+print(r"  \___|  \__, | |_.__/   \___| |_|      |_|\_\  \__,_| |_| |_|  \__,_| |_| |_| ")
+print(r"          __/ |                                                                ")
+print(r"         |___/                                                                 ")
+print(r"                                                                               ")
+    
+def send(target, counter, delay):
+    bombers = {
+    your phone was cracked
+    }
+
+    failed= 0
+    requested = 0
+    success= int(requested) - int(failed)
+    bombs = int(counter) + 1
+
+    while success != (int(bombs)):
+        banner()
+        api = random.choice(list(bombers))
+
+        print("===================CYBER KALLAN BOMBING SYSTEM====================")
+        print("                BOMBING in progress, please wait !!               ")
+        print("     Please keep your data connection active during bombing !!    ")
+        print("==================================================================")
+        print("             Target Number           : ", target)
+        print("             Number of Requests Sent : ", requested)
+        print("             Successful Requests     : ", success)
+        print("             Failed Requests         : ", failed)
+        print("             API used to BOMB Target : ", api)
+        print("==================================================================")
+        print(".         SUBSCRIBE CYBER KALLAN YOUTUBE CHANNEL FOR MORE         ")
+        print("              Use this for fun, not for revenge !!                ")
+        print("==================================================================")
+
+        result_url=str(bombers[api])+target
+
+        try:
+            requested = requested + 1
+            urllib.request.urlopen(str(result_url))
+            success = success + 1
+        except (urllib.error.HTTPError, urllib.error.URLError):
+            failed = failed + 1
+            continue
+
+        time.sleep(float(delay))
+
+banner()
+print("")
+send(input("Enter Target Number :  "), input("Enter Number of Messages: "), input("Enter Delay time (in seconds): "))
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# hap
-Ha
+# kingbomber
+Bomber tool for India
+## installation
+``` git clone https://github.com/cyberkallan/kingbomber
+cd kingbomber
+chmod +x *
+python kingbomber.py
+
+```
